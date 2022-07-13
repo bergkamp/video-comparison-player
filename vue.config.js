7fb3cc727b6121624ea5e3a2b289168b7d3a5584
Ruoshi <zruoshi@gmail.com>
2022-07-13 17:32:40 +0800

    mac build dmg file only

--- vue.config.js
+++ vue.config.js
@@ -2,40 +2,41 @@
 const webpack = require('webpack');
 
 module.exports = {
-  configureWebpack: {
-    devtool: 'source-map',
-    plugins: [
-      new webpack.DefinePlugin({
-        'process.env.FLUENTFFMPEG_COV': false
-      })
-    ]
-  },
-  pluginOptions: {
-    electronBuilder: {
-      productName: "Video comparison player",
-      nodeIntegration: true,
-      contextIsolation: false,
-      builderOptions: {
-        mac: {
-          icon: './src/icon/icon.icns',
-        },
-        win: {
-          icon: './src/icon/icon.png',
-          target: 'nsis'
-        },
-        nsis: {//https://www.electron.build/generated/nsisoptions
-          oneClick: false,
-          allowToChangeInstallationDirectory: true,
-          createDesktopShortcut: true
-        },
-        extraFiles: [
-          
-          {
-            from: "node_modules/@ffprobe-installer/${platform}-${arch}",
-            to: "static"
-          }
+    configureWebpack: {
+        devtool: 'source-map',
+        plugins: [
+            new webpack.DefinePlugin({
+                'process.env.FLUENTFFMPEG_COV': false
+            })
         ]
-      }
+    },
+    pluginOptions: {
+        electronBuilder: {
+            productName: "Video comparison player",
+            nodeIntegration: true,
+            contextIsolation: false,
+            builderOptions: {
+                mac: {
+                    target: 'dmg',
+                    icon: './src/icon/icon.icns'
+                },
+                win: {
+                    icon: './src/icon/icon.png',
+                    target: 'nsis'
+                },
+                nsis: {//https://www.electron.build/generated/nsisoptions
+                    oneClick: false,
+                    allowToChangeInstallationDirectory: true,
+                    createDesktopShortcut: true
+                },
+                extraFiles: [
+
+                    {
+                        from: "node_modules/@ffprobe-installer/${platform}-${arch}",
+                        to: "static"
+                    }
+                ]
+            }
+        }
     }
-  }
 }
